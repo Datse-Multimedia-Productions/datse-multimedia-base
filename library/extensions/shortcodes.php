6e65329a5b628f85cbe26a9bf50e51d91fabfb48
JigmeDatse <info@datsemultimedia.com>
2015-09-26 20:42:12 -0700

    .' vs '. error again...
    Looking at the diffs I see that I duplicated the error on another line.  It seems I am not doing good at getting the errors before I end up committing.  That's OK.

--- library/extensions/shortcodes.php
+++ library/extensions/shortcodes.php
@@ -36,7 +36,7 @@ function thematic_shortcode_framework_link() {
     }
     $themelink = '<a class="theme-link" href="'.$theme_uri.'" title="'.$theme_name.'" rel="home">'.$theme_name.'</a>';
     if ($parent_name) {
-        $themelink .= 'a child theme of <a class="theme-link" href="'.$parent_uri.'" title="'.$parent_name.'" rel="home">.'$parent_name.'</a>';
+        $themelink .= 'a child theme of <a class="theme-link" href="'.$parent_uri.'" title="'.$parent_name.'" rel="home">'.$parent_name.'</a>';
     }
     return apply_filters('thematic_theme_link',$themelink);
 }
