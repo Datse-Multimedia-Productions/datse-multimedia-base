52a2839f7aea9fe8596517a2182c6e1beca188bc
Jigme Datse Yli-Rasku <info@datsemultimedia.com>
2017-12-19 18:39:52 -0800

    Changed static content to theme functions
    Changed static content that pointed to Thematic to calls to
    `wp_get_theme()` call and results of the `WP_Theme` object.

--- library/extensions/theme-options.php
+++ library/extensions/theme-options.php
@@ -197,12 +197,11 @@ if (function_exists('childtheme_override_opt_page_help')) {
 	function thematic_opt_page_help() {	
 		
 		$screen = get_current_screen();
+
+		$theme = wp_get_theme();
 		
 		$sidebar  = '<p><strong>' . __( 'For more information:', 'datse-multimedia-base') . '</strong></p>';
-		$sidebar .= '<a href="http://thematictheme.com">ThematicTheme.com</a></p>';
-		$sidebar .= '<p><strong>' . __('For support:', 'datse-multimedia-base') . '</strong></p>';
-		$sidebar .= '<a href="http://thematictheme.com/forums/"> Thematic ';
-		$sidebar .= __('forums', 'datse-multimedia-base' ) . '</a></p>';
+		$sidebar .= '<a href='. $theme=>get('ThemeURI') . '">'. $theme=>get('Name') .'</a> </p>';
 		
 		$sidebar = apply_filters ( 'thematic_theme_opt_help_sidebar', $sidebar );
 		
